a72b2bc9bf247a7fe60ccb712288e2050c94be4e
Muhammad Ariananda <muharianandakd@gmail.com>
2023-11-18 15:05:31 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
+};
+
+init();
